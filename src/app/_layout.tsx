1d280a66271640fe3f5900a393930a123749be86
hndrr <yozhik20@gmail.com>
2024-12-30 21:05:40 +0900

    Remove unused tab components and screens from the project

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -12,7 +12,7 @@ import "react-native-reanimated";
 
 import { useColorScheme } from "hooks/useColorScheme";
 
-import "global.css";
+import "../global.css";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -36,7 +36,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
